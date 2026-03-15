92449f93e64241e57737f3a9060e168660753910
MatinoTekkit <MatinoTekkit@gmail.com>
2026-03-15 16:01:49 +0100

    add basic pricelist creation endpoint

---
+++ SportReservation/Controllers/PriceListController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportReservation.Middlewares;
+using SportReservation.Models;
+using SportReservation.Services;
+
+namespace SportReservation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PriceListController(PriceListService priceService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] PriceListCreateDto dto)
+    {
+        if (HttpContext.LoggedUser().Role != UserRole.Admin)
+        {
+            return Forbid();
+        }
+
+        var price = await priceService.Create(dto);
+        return Ok(price.ToDto());
+    }
+}
---
+++ SportReservation/Models/PriceListDtos.cs
@@ -0,0 +1,30 @@
+﻿namespace SportReservation.Models;
+
+public record PriceListDto(
+    Guid Id,
+    Guid FacilityTypeId,
+    DateTime ValidFrom,
+    DateTime? ValidTo,
+    decimal PricePerHour
+);
+
+public record PriceListCreateDto(
+    Guid FacilityTypeId,
+    DateTime ValidFrom,
+    DateTime? ValidTo,
+    decimal PricePerHour
+);
+
+public static class PriceListDtoExtensions
+{
+    public static PriceListDto ToDto(this PriceList type)
+    {
+        return new PriceListDto(
+            type.Id,
+            type.FacilityTypeId,
+            type.ValidFrom,
+            type.ValidTo,
+            type.PricePerHour
+        );
+    }
+}
