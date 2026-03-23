bd6418805cda3333c4349c0a9593dae56623ba72
MatinoTekkit <MatinoTekkit@gmail.com>
2026-03-23 20:24:22 +0100

    more changes

--- SportReservationUI/sportreservation/src/components/admin/AdminUsers.vue
+++ SportReservationUI/sportreservation/src/components/admin/AdminUsers.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import {secureFetch} from "@/auth.ts";
+
+const users = await secureFetch("/User").then(it => it.json())
 
 </script>
 
 <template>
-  <h1>Uživatelé</h1>
+  <div class="d-flex justify-lg-space-between">
+    <h1>Uživatelé</h1>
+    <v-btn icon="mdi-plus"/>
+  </div>
+
+  <v-card>
+    <v-table>
+      <thead>
+      <tr>
+        <th>Jméno</th>
+        <th>Email</th>
+        <th>Admin</th>
+        <th>Akce</th>
+      </tr>
+      </thead>
+      <tbody v-for="user in users">
+      <tr>
+        <td>{{ user.fullName }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.role == 'Admin' }}</td>
+        <td class="d-flex justify-lg-space-between">
+          <v-btn icon="mdi-pencil"/>
+          <v-btn icon="mdi-trash-can"/>
+        </td>
+      </tr>
+      </tbody>
+    </v-table>
+  </v-card>
 </template>
 
 <style scoped>
